91725d2c85829af29bc4f79193cf20897abd15e9
Vijay P V <pvvijayraj1998@gmail.com>
2021-07-23 14:14:59 +0530

    Dynamic notes
    Dynamic display of notes has been implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList"
 
 const App = () => {
+  const [notes, setNotes] = useState([{
+    id: nanoid(),
+    text: "This is the first note",
+    date: "15/2/21"
+  },
+  {
+    id: nanoid(),
+    text: "This is the second note",
+    date: "15/2/21"
+  },
+  {
+    id: nanoid(),
+    text: "This is the third note",
+    date: "15/2/21"
+  }]);
+
+
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes = {notes}/>
     </div>
   );
 };
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,11 @@
 import { MdDeleteForever } from 'react-icons/md';
 
-const Note = () => {
+const Note = ({ id, text, date }) => {
     return (
         <div className="note">
-            <span>Hello This the first note</span>
+            <span>{text}</span>
             <div className="note-footer">
-                <small>12/1/21</small>
+                <small>{date}</small>
                 <MdDeleteForever className="delete-icon" size="1.3em"/>
             </div>
         </div>
