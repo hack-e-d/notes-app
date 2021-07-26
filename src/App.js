6cbdaa128539af9ed8ad896164e8fa04eeae0e6f
Vijay P V <pvvijayraj1998@gmail.com>
2021-07-26 11:04:12 +0530

    Store and retrieve from localStorage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList"
 import Search from "./components/search";
 import Header from "./components/header";
 
 const App = () => {
-  const [notes, setNotes] = useState([{
-    id: nanoid(),
-    text: "This is the first note",
-    date: "15/2/21"
-  },
-  {
-    id: nanoid(),
-    text: "This is the second note",
-    date: "15/2/21"
-  },
-  {
-    id: nanoid(),
-    text: "This is the third note",
-    date: "15/2/21"
-  }]);
+  const [notes, setNotes] = useState([]);
 
   const [searchText , setSearchText ] = useState('');
 
   const [darkMode , setDarkMode ] = useState(false);
+
+  useEffect(() => {
+    const savedNotes = JSON.parse(localStorage.getItem('react-app-notes-app'));
+
+    if(savedNotes) {
+      setNotes(savedNotes);
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('react-app-notes-app',JSON.stringify(notes));
+  }, [notes]);
 
   const addNote = (text) => {
     // console.log(text);
